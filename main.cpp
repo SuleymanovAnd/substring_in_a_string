ca23bfd22f707dfbce41d6e7b0bc23a0f80c2b4e
Suleymanov Andrey <sasus37@gmail.com>
2022-07-07 21:05:17 +0300

    done

--- main.cpp
+++ main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
 
+bool substr ( char *x, char *z){
+    int countX = 0;
+    int countZ = 0;
+
+   for (int i = 0; *(x+i) != '\0'; i++) {countX ++;}
+   for (int i = 0; *(z+i) != '\0' ; i++) {countZ ++;}
+
+   for (int i =0; i < countX; i++){
+       if (*(x+i) == *(z)){
+           for (int j =0; j<countZ;j++){
+               if (*(x+i+j) != *(z+j)){break;}
+               if (j == countZ-1){ return true;}
+           }
+       }
+   }
+   return false;
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    char* a = "Hello world";
+
+    char* b = "wor";
+
+    char* c = "banana";
+
+    std::cout << substr(a,b) << " " << substr(a,c);
 }
